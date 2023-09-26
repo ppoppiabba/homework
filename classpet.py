d9b90b313f0661d4b033778b30977223a84f21ae
ppoppiabba <knangkong@naver.com>
2023-09-26 09:51:59 +0900

    수정

--- classpet.py
+++ classpet.py
@@ -1,12 +1,16 @@
+
 class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def __str__(self):
-        return f"Name: {self.name}, Age: {self.age}"
+        return f"이름: {self.name}, 나이: {self.age}"
 
 class Dog(Pet):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
     def bark(self, n):
         for _ in range(n):
             print("bark!")
@@ -15,6 +19,9 @@ class Dog(Pet):
         return self.age * 4
 
 class Cat(Pet):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
     def meow(self, n):
         for _ in range(n):
             print("meow~")
@@ -23,12 +30,19 @@ class Cat(Pet):
         return self.age * 4
 
 
+bark_count = int(input("짖을 횟수: "))
+dog_name = input("Name: ")
+dog_age = int(input("Age: "))
+my_dog = Dog(dog_name, dog_age)
+my_dog.bark(bark_count)
+print(my_dog)
+print("사람 나이로 환산:", my_dog.human_age())
 
-popo = Dog("popo", 10)  
-popo.bark(3) 
-print(popo)  
-print("사람 나이로 환산:", popo.human_age())  
-pipi = Cat("pipi", 5)  
-pipi.meow(4)
-print(pipi) 
-print("사람 나이로 환산:", pipi.human_age())  
+
+meow_count = int(input("울을 횟수: "))
+cat_name = input("Name: ")
+cat_age = int(input("Age: "))
+my_cat = Cat(cat_name, cat_age)
+my_cat.meow(meow_count)
+print(my_cat)
+print("사람 나이로 환산:", my_cat.human_age())
